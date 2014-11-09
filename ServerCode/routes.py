3e4f4da822f4433038cd4db54bf214ed47c9cede
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-11-09 18:22:46 +0000

    simple upload handler

--- ServerCode/routes.py
+++ ServerCode/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import request
+
+uploadDirectory = 'uploads'
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    pass
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(uploadDirectory+'/'+f.filename)
+    return ''
