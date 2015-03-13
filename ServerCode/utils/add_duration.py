6db8c962b84e801916b7e754b764791d23051688
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-03-13 03:32:44 +0000

    fixed script

--- ServerCode/utils/add_duration.py
+++ ServerCode/utils/add_duration.py
@@ -1,4 +1,5 @@
 import os
+import socket
 import psycopg2
 import sys
 sys.path.append('..')
@@ -6,7 +7,7 @@ import track_details
 
 
 db = 'fyp'
-user, password = ('dev', 'devPass') if os.name == 'nt' else ('fypuser', 'fypPass')
+user, password = ('dev', 'devPass') if socket.gethostname() == 'g50-70' else ('fypuser', 'fypPass')
 host = 'localhost'
 
 
@@ -17,8 +18,11 @@ cursor = conn.cursor()
 cursor.execute("SELECT file_path FROM tracks")
 
 for row in list(cursor.fetchall()):
-    track = track_details.load_from_file(os.path.join('uploads', row[0])
-    cursor.execute("update tracks set duration = %s where file_path = %s", (row[0],))
+    fp = row[0]
+    full_fp = os.path.join('C:\\users\\matthew\\desktop', 'uploads', fp)
+    track = track_details.load_from_file(full_fp)
+
+    cursor.execute("update tracks set duration = %s where file_path = %s", (track.duration, fp))
 
 
 conn.commit()
